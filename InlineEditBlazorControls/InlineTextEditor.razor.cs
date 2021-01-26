92830bc0b625ec2699445d68bc02b21fd81eb296
Marcel Siegel <marcel.siegel@taetzschwitz.de>
2021-01-26 15:25:53 +0100

    first TextEditor

---
+++ InlineEditBlazorControls/InlineTextEditor.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace InlineEditBlazorControls
+{
+    public partial class InlineTextEditor : ComponentBase
+    {
+        public bool EditMode { get; set; } = false;
+
+        [Parameter]
+        public EventCallback<Tuple<string, int?>> ValueSaved { get; set; }
+
+        [Parameter]
+        public string EditableValue { get; set; } = "";
+
+        [Parameter]
+        public int EditableValueId { get; set; }
+
+        void ActivateEditMode()
+        {
+            EditMode = true;
+        }
+
+        void SaveValue()
+        {
+            ValueSaved.InvokeAsync(new Tuple<string, int?>(EditableValue, EditableValueId));
+            EditMode = false;
+        }
+
+        void CancelEditMode()
+        {
+            EditMode = false;
+        }
+    }
+}
