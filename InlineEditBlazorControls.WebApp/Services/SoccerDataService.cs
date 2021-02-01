8bf810b6c7d3b0704316a191d7fa543cd909ab18
Marcel Siegel <marcel.siegel@taetzschwitz.de>
2021-02-01 22:10:18 +0100

    Demo-Page

---
+++ InlineEditBlazorControls.WebApp/Services/SoccerDataService.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace InlineEditBlazorControls.WebApp.Services
+{
+    public class SoccerDataService
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        public SoccerDataService()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "InlineEditBlazorControls");
+
+            var baseUrl = "https://api.openligadb.de";
+            client.BaseAddress = new System.Uri(baseUrl.EndsWith("/") ? baseUrl : string.Concat(baseUrl, "/"));
+        }
+
+        public async Task<List<Match>> GetMatches()
+        {
+            string leagueShortcut = "bl1";
+            int season = 2020;
+
+            string path = string.Concat("getmatchdata/", leagueShortcut, "/", season,"/",1);
+
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringresult = await response.Content.ReadAsStringAsync();
+                var res = JsonConvert.DeserializeObject<List<Match>>(stringresult);
+                return res;
+            }
+
+            return null;
+        }
+    }
+
+    public record Match
+    {
+        public int MatchId { get; set; }
+
+        public DateTime? MatchDateTime { get; set; }
+
+        public string LeagueName { get; set; }
+
+        public Team Team1 { get; set; }
+
+        public Team Team2 { get; set; }
+
+        public Group Group { get; set; }
+    }
+
+    public class Group
+    {
+        public string GroupName { get; set; }
+        public int GroupOrderID { get; set; }
+        public int GroupID { get; set; }
+    }
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public string ShortName { get; set; }
+
+        public string TeamIconUrl { get; set; }
+        public string TeamGroupName { get; set; }
+    }
+}
--- InlineEditBlazorControls.WebApp/Startup.cs
+++ InlineEditBlazorControls.WebApp/Startup.cs
@@ -1,4 +1,4 @@
-using InlineEditBlazorControls.WebApp.Data;
+using InlineEditBlazorControls.WebApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +28,7 @@ namespace InlineEditBlazorControls.WebApp
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<SoccerDataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
